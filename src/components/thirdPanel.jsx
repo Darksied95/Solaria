a063bb1f60af7281950bc343a937c4ab262d80c2
Darksied95 <aliyuefe9501@gmail.com>
2023-03-15 22:52:09 +0100

    addedstyling to thirdPanel

--- src/components/thirdPanel.jsx
+++ src/components/thirdPanel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AiFillCheckCircle, AiFillMedicineBox } from "react-icons/ai";
 import { FaDna } from "react-icons/fa";
 import { BsVirus } from "react-icons/bs";
-import { GiHumanPyramid } from "react-icons/gi";
+import { GiHumanTarget } from "react-icons/gi";
 import { FiMoreHorizontal } from "react-icons/fi";
 
 const ThirdPanel = () => {
@@ -11,7 +11,8 @@ const ThirdPanel = () => {
       heading: "Multispecific liquids analysis",
       subHeading: "Next-gen immuno",
       time: "5d",
-      iconColor: "#f3d9c3",
+      backgroundColor: "#f3d9c3",
+      color: "#000",
       Icon: "1",
       text: "New feedback",
     },
@@ -19,7 +20,8 @@ const ThirdPanel = () => {
       heading: "Protein characterization during childhood and pregnancy",
       subHeading: "Harmony",
       time: "1h",
-      iconColor: "#3d3d3d",
+      backgroundColor: "#3d3d3d",
+      color: "white",
       Icon: "!",
       text: "Deadline is today",
     },
@@ -27,7 +29,8 @@ const ThirdPanel = () => {
       heading: "Quick element dissolution testing",
       subHeading: "Pharmaceutical",
       time: "7h",
-      iconColor: "#b7c9cc",
+      backgroundColor: "#b7c9cc",
+      color: "white",
       Icon: AiFillCheckCircle,
       text: "Accepted",
     },
@@ -38,32 +41,32 @@ const ThirdPanel = () => {
       heading: "Viruses",
       progress: "96",
       Icon: BsVirus,
-      color: "#627697",
+      color: (alpha) => `rgba(92, 113, 143, ${alpha || 255})`,
     },
     {
       heading: "Bacterial cells",
       progress: "33",
       Icon: FaDna,
-      color: "#f5bf92",
+      color: (alpha) => `rgba(245, 191, 146, ${alpha || 255})`,
     },
     {
       heading: "Med Science",
       progress: "7",
       Icon: AiFillMedicineBox,
-      color: "#e3b2b7",
+      color: (alpha) => `rgba(223, 172, 176, ${alpha || 255})`,
     },
     {
       heading: "Human gens",
       progress: "58",
-      Icon: GiHumanPyramid,
-      color: "#9dafb4",
+      Icon: GiHumanTarget,
+      color: (alpha) => `rgba(167, 182, 186, ${alpha || 255})`,
     },
   ];
 
   return (
-    <div>
-      <div>
-        <h1>Monthly pinned</h1>
+    <div className="w-[500px] outline">
+      <div className="flex justify-between">
+        <h1 className="font-bold  text-lg">Monthly pinned</h1>
         <p>View all</p>
       </div>
       <div>
@@ -71,39 +74,56 @@ const ThirdPanel = () => {
           let { subHeading, time, Icon, iconColor, heading, text } = each;
 
           return (
-            <div key={index}>
-              <div>
-                <h2>{subHeading}</h2>
+            <div key={index} className="border border-black mb-4 p-4">
+              <div className="flex justify-between">
+                <h2 className="mb-1">{subHeading}</h2>
                 <span>{time}</span>
               </div>
-              <h1>{heading}</h1>
-              <div>
-                <div>{typeof Icon === "string" ? Icon : <Icon />}</div>
+              <h1 className="font-bold max-w-[70%] mb-3">{heading}</h1>
+              <div className="flex items-center  gap-2">
+                {typeof Icon === "string" ? (
+                  <p
+                    className={` text-xs w-4 text-center rounded-full aspect-square font-bold`}
+                    style={{
+                      backgroundColor: each.backgroundColor,
+                      color: each.color,
+                    }}
+                  >
+                    {Icon}
+                  </p>
+                ) : (
+                  <Icon color={each.backgroundColor} fontSize={18} />
+                )}
                 <p>{text}</p>
               </div>
             </div>
           );
         })}
       </div>
-      <div>
-        <p>
-          My learnigs <br />
-          <span>Your progress of medical lectures</span>
+      <div className="flex justify-between mb-4">
+        <p className="font-semibold text-lg leading-none">
+          My learnings <br />
+          <span className="opacity-70 text-xs font-normal">
+            Your progress of medical lectures
+          </span>
         </p>
         <FiMoreHorizontal />
       </div>
 
-      <div>
+      <div className="flex flex-wrap gap-4">
         {medicalLecturesProgress.map((each, index) => {
           const Icon = each.Icon;
           return (
-            <div key={index}>
-              <div>
-                <Icon />
+            <div key={index} className="w-[40%] flex items-center ">
+              <div
+                className="aspect-square h-8 grid place-content-center "
+                style={{ backgroundColor: each.color(0.15) }}
+              >
+                <Icon color={each.color()} fontSize={16} />
               </div>
-              <div>
-                <h2>{each.heading}</h2>
-                <p>{each.progress}% progress</p>
+              <div className="ml-3">
+                <h2 className="font-bold text-sm ">{each.heading}</h2>
+                <p className="text-xs">{each.progress}% progress</p>
               </div>
             </div>
           );
