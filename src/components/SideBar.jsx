a9c44d6e3e53225f98cc79a641ea7219844b5f94
Darksied95 <aliyuefe9501@gmail.com>
2023-03-17 11:38:00 +0100

    updated sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,7 +3,10 @@ import { MdOutlineBarChart } from "react-icons/md";
 import { TbChartBubbleFilled } from "react-icons/tb";
 import { IoIosCopy, IoIosSettings } from "react-icons/io";
 import { BiChevronDown } from "react-icons/bi";
+import { BsFillPencilFill } from "react-icons/bs";
+import { RiMessage2Fill } from "react-icons/ri";
 import { CgClose } from "react-icons/cg";
+import { FiLogOut } from "react-icons/fi";
 import ResourcesDetails from "./ResourcesDetails";
 import Button from "./Button";
 import {
@@ -24,7 +27,10 @@ const SideBar = ({ showSideBar, closeSideBar }) => {
       } `}
     >
       <div className="flex items-center w-full justify-between mt-10">
-        <h2 className="font-bold text-3xl uppercase ">solarius</h2>
+        <div>
+          <h1 className="font-bold text-2xl text-center">Dr. Riddle</h1>
+          <p className="text-center text-xl">Scientist</p>
+        </div>
         <CgClose size={40} onClick={closeSideBar} className="cursor-pointer" />
       </div>
       <Button
@@ -86,6 +92,27 @@ const SideBar = ({ showSideBar, closeSideBar }) => {
         <IoIosSettings size={23} />
         <p>Settings</p>
       </Button>
+
+      <div className="flex  items-center gap-10 mr-3 lg:block lg:mx-auto  w-full">
+        <div className="w-9 aspect-square bg-white mb-4  rounded-full grid place-content-center  cursor-pointer">
+          <BsFillPencilFill />
+        </div>
+        <div className="w-9 aspect-square bg-white mb-4 rounded-full grid place-content-center cursor-pointer">
+          <RiMessage2Fill />
+        </div>
+        <p className="w-9 aspect-square bg-black text-white mb-4 rounded-full grid place-content-center text-xl font-bold cursor-pointer">
+          <span className="-mt-1 ">+</span>
+        </p>
+      </div>
+      <button
+        className="flex items-center gap-3 font-bold text-lg mb-10 w-full p-3 mt-auto"
+        onClick={() => window.close()}
+      >
+        <div className="bg-black p-2 rounded-full">
+          <FiLogOut color="white" />
+        </div>
+        <p>Log out</p>
+      </button>
     </div>
   );
 };
